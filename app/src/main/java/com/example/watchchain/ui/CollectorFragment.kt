272ef18f7313f0ac9b4bb0413cb0c064e0bcf8a4
Alexey <allxyog@gmail.com>
2022-09-20 16:32:58 +0200

    hinzugefügt

--- app/src/main/java/com/example/watchchain/ui/CollectorFragment.kt
+++ app/src/main/java/com/example/watchchain/ui/CollectorFragment.kt
@@ -1,7 +1,15 @@
 package com.example.watchchain.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.watchchain.R
+import com.example.watchchain.adapter.BrowserAdapter
+import com.example.watchchain.adapter.CollectorAdapter
 import com.example.watchchain.databinding.FragmentCollectorBinding
 
 class CollectorFragment : Fragment() {
@@ -9,5 +17,19 @@ class CollectorFragment : Fragment() {
     private lateinit var binding: FragmentCollectorBinding
 
     private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_collector, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.nftsRecycler.adapter = CollectorAdapter()
+    }
 
 }
--- app/src/main/java/com/example/watchchain/ui/CreatorProfileFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.watchchain.ui
-
-import androidx.fragment.app.Fragment
-
-class CreatorProfileFragment : Fragment() {
-}
